8a46e44d811cddea4fd56bc8bff04ef0a6bf873a
NikitaBumblebee <nikitashmelev7@gmail.com>
2022-03-10 21:13:13 +0700

    Initial Commit

---
+++ number-generator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  number-generator
+//
+//  Created by Nikita Shmelev on 3/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ number-generator/number_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  number_generatorApp.swift
+//  number-generator
+//
+//  Created by Nikita Shmelev on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct number_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
